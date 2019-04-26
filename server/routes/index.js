d2410be0dac47f2f5289b19c136a44b362663532
bzargesg <bzargesg@gmail.com>
2019-04-26 12:23:07 -0600

    build reddis cache

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,38 @@
 const router = require('express').Router();
+const redis = require('redis');
 const db = require('../database/mongoQueryDB');
 
+const client = redis.createClient();
+client.on('connect', () => {
+  console.log('Redis client connected');
+});
+client.on('error', () => {
+  console.log('redis failed to connect');
+});
 router.get('/restaurants/:hotelName', (req, res) => {
   const { hotelName } = req.params;
-  db.attractions.getAttractions(hotelName, 'restaurants').then((restaurantsInRange) => {
-    res.json(restaurantsInRange);
+  client.get(`rest:${hotelName}`, (err, restaurants) => {
+    if (!restaurants) {
+      db.attractions.getAttractions(hotelName, 'restaurant').then((restaurantsInRange) => {
+        client.set(`rest:${hotelName}`, JSON.stringify(restaurantsInRange));
+        res.json(restaurantsInRange);
+      });
+    } else {
+      res.json(JSON.parse(restaurants));
+    }
   });
 });
 router.get('/attractions/:hotelName', (req, res) => {
   const { hotelName } = req.params;
-  db.attractions.getAttractions(hotelName, 'attraction').then((restaurantsInRange) => {
-    res.json(restaurantsInRange);
+  client.get(`attr:${hotelName}`, (err, attractions) => {
+    if (!attractions) {
+      db.attractions.getAttractions(hotelName, 'attraction').then((restaurantsInRange) => {
+        client.set(`attr:${hotelName}`, JSON.stringify(restaurantsInRange));
+        res.json(restaurantsInRange);
+      });
+    } else {
+      res.json(JSON.parse(attractions));
+    }
   });
 });
 router.get('/hotels/:hotelName', (req, res) => {
@@ -21,3 +43,17 @@ router.get('/hotels/:hotelName', (req, res) => {
 // router.get('/initialize', controller.distanceMatrix.initialize);
 
 module.exports = router;
+// const { hotelName } = req.params;
+// client.get(hotelName, (err, restaurants) => {
+//   if (restaurants) {
+//     console.log('got rest', restaurants);
+//     res.json(JSON.parse(restaurants));
+//   } else {
+//     db.attractions.getAttractions(hotelName, 'restaurants').then((restaurantsInRange) => {
+//       console.log('did not get rest', restaurantsInRange);
+
+//       // client.set(hotelName, JSON.stringify(restaurantsInRange));
+//       res.json(restaurantsInRange);
+//     });
+//   }
+// });
